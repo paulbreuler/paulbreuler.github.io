0bc5a72373522cc5c7441cfa87f5420b225d664f
SudoSandwichX <paul.jbre@gmail.com>
2016-04-04 17:00:25 -0500

    JS connected and formatted CSS
    - JS slideshow working
    - Added JQuery 1.12.2 minified version

--- Scripts/myscripts.js
+++ Scripts/myscripts.js
@@ -1,14 +1,18 @@
-var myIndex = 0;
-carousel();
+$(window).load(function () {
+    var myIndex = 0;
+    carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("my-slides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+    // Image slideshow
+    function carousel() {
+        var i;
+        var x = $(".main-img");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) { myIndex = 1 }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(carousel, 6000);
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 6000);    
-}
+
+});
